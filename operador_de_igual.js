c30488930ccbb60346d10ba6891c96b51ee314bc
joaoleandro <new.joaoleandro@gmail.com>
2024-02-10 16:34:15 -0300

    operador de igualdade

---
+++ operador_de_igual.js
@@ -0,0 +1,10 @@
+const num1 = 10
+const num2 = "10"
+console.log(num1 == num2)
+// resultado =  true
+
+// Como corrigir, ultilizando o operador === 
+const a = 20
+const b = "20"
+console.log(a === b)
+// resultado = false
