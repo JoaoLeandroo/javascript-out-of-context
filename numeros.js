d1b60e0c2a97943b9c247cbe86dc5af7ccd31169
joaoleandro <new.joaoleandro@gmail.com>
2024-02-10 16:24:35 -0300

    numeros string

---
+++ numeros.js
@@ -0,0 +1,10 @@
+const numero = "20"
+const soma = numero + 2
+console.log(soma)
+// soma = 202
+
+// Como corrigir isso - convertendo a string para number
+const numero1 = parseInt("20")
+const soma2 = numero1 + 2
+console.log(soma2)
+// soma2 = 22
